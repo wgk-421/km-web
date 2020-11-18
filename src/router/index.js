8635d790f7da3b444b70de82e3e21af2b5f130fe
wgk <wanggaokun@wo.cn>
2020-11-19 01:08:04 +0800

    1.安装sass 2.添加校验文件 3.添加登陆页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,37 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: () => import('@/components/HelloWorld')
-    }
-  ]
+/**
+ * fixedRoutes 固定路由存放
+ */
+const fixedRoutes = [
+  {
+    path: '',
+    name: 'home',
+    component: () => import('@/components/HelloWorld'),
+    hidden: true
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/page/login'),
+    hidden: true
+  }
+]
+
+const createRouter = () => new Router({
+  // mode: 'history', //需要后端服务支持
+  scrollBehavior: () => ({ y: 0 }),
+  routes: fixedRoutes
 })
+
+const router = createRouter()
+
+// 导出重置路由方法
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+// 导出路由
+export default router
